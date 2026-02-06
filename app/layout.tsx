e5815b5ce3708b06ebac3e3d5801056345601951
Павло Глущенко <paha@MacBook-Air-Pavlo.local>
2026-02-06 11:48:03 +0200

    fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export const viewport = {
 };
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -39,6 +40,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </Script>
           </>
         )}
+        {FB_PIXEL_ID && (
+          <>
+            <Script id="fb-pixel" strategy="afterInteractive">
+              {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '${FB_PIXEL_ID}');
+        fbq('track', 'PageView');
+      `}
+            </Script>
+
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
+        )}
+
       </body>
     </html>
   );
